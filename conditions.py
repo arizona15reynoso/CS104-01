33e4f45105702ab17c5d6e363feb5c33af24f951
arizona15reynoso <98927151+arizona15reynoso@users.noreply.github.com>
2022-02-23 15:36:58 -0500

    Conditions.py

---
+++ conditions.py
@@ -0,0 +1,11 @@
+x = int (input ('Please enter the current temperature: '))
+if x > 90 :
+  print ("Wear shorts")
+elif x > 70 :
+  print ("Short sleeves are fine")
+elif x > 50 :
+  print ("Wear a jacket")
+elif x > 32 :
+  print ("Wear a heavy coat")
+else :
+  print ("Stay inside")
